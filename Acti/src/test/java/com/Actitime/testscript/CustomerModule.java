6b47e8b1da23acc410b31929865f2bdc2e67ab66
LENOVO <LENOVO@DESKTOP-I3RSF9F>
2024-01-05 13:14:43 +0530

    First Commit

---
+++ Acti/src/test/java/com/Actitime/testscript/CustomerModule.java
@@ -0,0 +1,13 @@
+package com.Actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+	@Test
+public void createCustomer() {
+		Reporter.log("Customer",true);
+		
+	
+}
+}
